4702c3755f31e00be17a2f5dc39c5b706ff5dbb5
Hack Reactor Students <github@hackreactor.com>
2014-01-07 15:32:52 -0800

    Fills in 'sortBy' function

--- src/underbar.js
+++ src/underbar.js
@@ -360,6 +360,14 @@ var _ = {};
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    collection.sort(function(a, b) {
+      if(typeof iterator === 'function') {
+        return iterator(a) - iterator(b);
+      } else {
+        return a[iterator] - b[iterator];
+      }
+    });
+    return collection;
   };
 
   // Zip together two or more arrays with elements of the same index
