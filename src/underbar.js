13e9a87abed8886ea598d0cd7872cbd529cd8984
Hack Reactor Students <github@hackreactor.com>
2014-01-07 11:19:37 -0800

    Fills in 'invoke' function

--- src/underbar.js
+++ src/underbar.js
@@ -146,6 +146,15 @@ var _ = {};
   // Calls the method named by methodName on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(val){
+      var res;
+      if(typeof functionOrKey === 'function') {
+        res = functionOrKey.apply(val,args);
+      } else {
+        res = val[functionOrKey].apply(val,args);
+      }
+      return res;
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
