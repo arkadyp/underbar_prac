e984a0f9c55ef927d54ea7c7a3a27903e07aa932
Hack Reactor Students <github@hackreactor.com>
2014-01-07 13:52:49 -0800

    Fills in 'extend' function

--- src/underbar.js
+++ src/underbar.js
@@ -209,7 +209,7 @@ var _ = {};
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
     if(iterator === undefined) {
-      iterator = function(val){return Boolean(val)};
+      iterator = function(val){return Boolean(val);};
     }
 
     return !_.every(collection, function(val) {
@@ -237,6 +237,12 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++){
+      for (var key in arguments[i]){
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
